06296777db5ad4ab0ac0c55e02b7fb9a026f41f1
Yan Fung <fungmingyan@hotmail.com>
2023-06-11 11:26:30 +0100

    Modified the test with test passing by rending the app correctly

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from '../components/App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "../components/App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders app", () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
