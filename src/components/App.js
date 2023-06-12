87e8bd861f8b7f1f557dea70258e3f102ede17ee
Yan Fung <fungmingyan@hotmail.com>
2023-06-12 09:04:48 +0100

    Implemented various route paths for login, signup and profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
-import '../styles/App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import IsAnonymous from "./IsAnonymous";
+import IsPrivate from "./IsPrivate";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <h1>FOODWISE APP</h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route
+          path="/login"
+          element={
+            <IsAnonymous>
+              <Login />
+            </IsAnonymous>
+          }
+        ></Route>
+        <Route
+          path="/signup"
+          element={
+            <IsAnonymous>
+              <Signup />
+            </IsAnonymous>
+          }
+        ></Route>
+        <Route
+          path="/profile"
+          element={
+            <IsPrivate>
+              <Profile />
+            </IsPrivate>
+          }
+        ></Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
