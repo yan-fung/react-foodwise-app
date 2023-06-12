5a08461cc81411902aa8c40b989ad4f11b13b7e1
Yan Fung <fungmingyan@hotmail.com>
2023-06-12 08:34:43 +0100

    Added home and profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+
+
+const Profile = () => {
+  return <div>Profile</div>;
+};
+
+export default Profile;
