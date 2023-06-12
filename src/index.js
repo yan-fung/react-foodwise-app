8d216498a925a473be71d52f2ee1c2d79547c2b3
Yan Fung <fungmingyan@hotmail.com>
2023-06-12 08:32:14 +0100

    Wrapped the entire app inside the AuthProviderWrapper

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
+import { AuthProviderWrapper } from "./context/auth.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProviderWrapper>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProviderWrapper>
   </React.StrictMode>
 );
