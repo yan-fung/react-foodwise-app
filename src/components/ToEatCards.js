adbb5313544403c1fa8b1e600faae4c8514fa0f1
Yan Fung <fungmingyan@hotmail.com>
2023-06-16 22:57:11 +0100

    Refactor on-click function to improve performance

--- src/components/ToEatCards.js
+++ src/components/ToEatCards.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const ToEatCards = ({ text, todoId, onRemoveTodo }) => {
+const ToEatCards = ({ text, todoId, onRemoveTodo, onIncreaseWastedClick }) => {
   return (
     <div className="toeat">
       {text}
+      <button type="button" onClick={() => onIncreaseWastedClick(todoId)}>
+        Wasted
+      </button>
       <button type="button" onClick={() => onRemoveTodo(todoId)}>
         Delete
       </button>
