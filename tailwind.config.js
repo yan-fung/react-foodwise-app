43305fafe25de82093dd3d927e1f216930142ae5
Yan Fung <fungmingyan@hotmail.com>
2023-06-29 21:31:33 +0100

    Added colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#2CBCE9",
+        red: "DC4492",
+        yellow: "#FDCC49",
+        grey: "EDEDED",
+        green: "#AFF91C",
+        "deep-blue": "#010026",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)",
+        brown: "#3e2723",
+        "dark-pink": "rgb(189, 171, 175)",
+        "darker-pink": "rgb(207, 192, 195)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
