623a9c3b3052fb6da2f8452ed42f6df66698d714
Yan Fung <fungmingyan@hotmail.com>
2023-06-30 18:10:29 +0100

    Added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,24 @@ module.exports = {
         "dark-pink": "rgb(189, 171, 175)",
         "darker-pink": "rgb(207, 192, 195)",
       },
+      keyframes: {
+        headShake: {
+          "0%": { transform: "translateX(0)" },
+          "6.5%": { transform: "translateX(-6px) rotateY(-9deg)" },
+          "18.5%": { transform: "translateX(5px) rotateY(7deg)" },
+          "31.5%": { transform: "translateX(-3px) rotateY(-5deg)" },
+          "43.5%": { transform: "translateX(2px) rotateY(3deg)" },
+          "50%": { transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        headShake: "headShake 2s infinite",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
     },
   },
   plugins: [require("flowbite/plugin")],
