fe201ad38042d316423698e9aa89805b40c516be
Yan Fung <fungmingyan@hotmail.com>
2023-06-30 18:24:46 +0100

    Added styling and images to the weekly food waste count

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import apple from "../assets/apple.png";
+import onion from "../assets/high.gif";
+import garlic from "../assets/low.gif";
 
 const Home = ({ wastedNum, countWastedFood }) => {
   useEffect(() => {
@@ -6,11 +9,47 @@ const Home = ({ wastedNum, countWastedFood }) => {
   }, []);
 
   return (
-    <div className="home">
-      <h1>Weekly Wasted Number</h1>
-
-      {wastedNum}
-    </div>
+    <>
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <div style={{ position: "relative" }} className="pt-20">
+            <img src={apple} />
+            <div
+              style={{
+                position: "absolute",
+                top: "35%",
+                left: "40%",
+                transform: "translate(-30%, -50%)",
+              }}
+              className="text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-teal-800"
+            >
+              <h1>Weekly Wasted Number</h1>
+              {wastedNum}
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                top: "45%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+              className="pt-20"
+            >
+              <img
+                src={wastedNum > 1 ? onion : garlic}
+                className="animate-headShake w-300 h-60"
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    </>
   );
 };
 
