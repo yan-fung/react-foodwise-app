d3cb615dc7897d8b7a14a83db591d30800a821d7
Yan Fung <fungmingyan@hotmail.com>
2023-07-01 23:43:34 +0100

    Adjusted the position of the images

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,40 +11,42 @@ const Home = ({ wastedNum, countWastedFood }) => {
   return (
     <>
       <>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            height: "100vh",
-          }}
-        >
-          <div style={{ position: "relative" }} className="pt-20">
-            <img src={apple} />
-            <div
-              style={{
-                position: "absolute",
-                top: "35%",
-                left: "40%",
-                transform: "translate(-30%, -50%)",
-              }}
-              className="text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-teal-400"
-            >
-              <h1>Weekly Wasted Number</h1>
-              {wastedNum}
-            </div>
-            <div
-              style={{
-                position: "absolute",
-                top: "45%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-              }}
-              className="pt-20"
-            >
-              <img
-                src={wastedNum > 1 ? onion : garlic}
-                className="animate-headShake w-300 h-60"
-              />
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              height: "100vh",
+            }}
+          >
+            <div style={{ position: "relative" }} className="pt-20">
+              <img src={apple} />
+              <div
+                style={{
+                  position: "absolute",
+                  top: "35%",
+                  left: "40%",
+                  transform: "translate(-30%, -50%)",
+                }}
+                className="text-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-600 to-teal-400"
+              >
+                <h1>Weekly Wasted Number</h1>
+                {wastedNum}
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  top: "45%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                }}
+                className="pt-20"
+              >
+                <img
+                  src={wastedNum > 1 ? onion : garlic}
+                  className="animate-headShake w-300 h-60"
+                />
+              </div>
             </div>
           </div>
         </div>
