a504549e6fe9ef6a412ec9e50e79694ab41a7bb2
Yan Fung <fungmingyan@hotmail.com>
2023-07-02 14:53:21 +0100

    Updated grid display to include the remaining days of the expiry date for food items. Also, improved layout for displaying the food item, expiry date info and waste/consumed buttons

--- src/components/ToEatCards.js
+++ src/components/ToEatCards.js
@@ -1,24 +1,56 @@
 import React from "react";
 import { Button } from "flowbite-react";
 
-const ToEatCards = ({ text, todoId, onRemoveTodo, onIncreaseWastedClick }) => {
-  return (
-    <div className="flex pb-3 items-center justify-center">
-      <span className="text-xl">{text}</span>
-      <div className="pr-3 pl-3">
-        <Button
-          className="bg-rose-400 hover:bg-rose-500"
-          type="button"
-          onClick={() => onIncreaseWastedClick(todoId)}
-        >
-          Wasted
-        </Button>
-      </div>
+const ToEatCards = ({
+  text,
+  todoId,
+  expirydate,
+  onRemoveTodo,
+  onIncreaseWastedClick,
+}) => {
+  // Define the expiry date
+  const expiryDate = new Date(expirydate);
 
-      <Button type="button" onClick={() => onRemoveTodo(todoId)}>
-        Consumed
-      </Button>
-    </div>
+  // Get today's date
+  const today = new Date();
+
+  // Calculate the time difference in milliseconds
+  const timeDiff = expiryDate.getTime() - today.getTime();
+
+  // Convert the time difference to days
+  const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+  return (
+    <>
+      <>
+        <div className="pb-3 items-center justify-left">
+          <div className="grid grid-cols-4">
+            <div className="text-md">{text}</div>
+            {typeof daysRemaining === "number" && !isNaN(daysRemaining) ? (
+              <>
+                <span className="text-center">{daysRemaining}</span>
+                <div className="pr-3 pl-3">
+                  <Button
+                    className="bg-rose-400 hover:bg-rose-500 w-16"
+                    type="button"
+                    onClick={() => onIncreaseWastedClick(todoId)}
+                  >
+                    Wasted
+                  </Button>
+                </div>
+                <Button
+                  className="w-20"
+                  type="button"
+                  onClick={() => onRemoveTodo(todoId)}
+                >
+                  Consumed
+                </Button>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </>
+    </>
   );
 };
 
