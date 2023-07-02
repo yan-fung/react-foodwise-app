f95e4522877bd84622b45d1739e45f4fcef4d5a1
Yan Fung <fungmingyan@hotmail.com>
2023-07-02 15:04:35 +0100

    Refined the DatePicker component and implemented a grid layout with a header for displaying the food items

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -62,7 +62,10 @@ const Profile = ({
   return (
     <>
       <div className="text-center p-5">
-        <h1>List of Foods</h1>
+        <h1 className="font-bold text-amber-950">Less waste, more taste.</h1>
+        <span className="font-bold text-amber-950">
+          Embrace a sustainable food culture 🍲
+        </span>
       </div>
       <div className="flex flex-col items-center">
         <form onSubmit={handleSubmit} className="pb-5 flex items-center">
@@ -75,21 +78,29 @@ const Profile = ({
             onChange={handleText}
             className="pr-2"
           />
+          <div className="pr-2">
+            <DatePicker
+              className="text-sm text-center w-28 h-10 border-pink-200 hover:border-cyan-300 rounded-md text-gray-900"
+              selected={selectedDate}
+              onChange={handleDateChange}
+            />
+          </div>
+
           <Button gradientDuoTone="purpleToBlue" size="sm" type="submit">
             Add
           </Button>
         </form>
-        <DatePicker
-          className="text-center w-28 rounded-md"
-          selected={selectedDate}
-          onChange={handleDateChange}
-        />
+        <div className="grid grid-cols-4 pb-4" style={{ width: "350px" }}>
+          <div className="text-md font-bold">Items</div>
+          <div className="text-md font-bold text-center">Expires in (days)</div>
+        </div>
         <div className="flex flex-col">
           {search.map((todo) => (
             <div key={todo._id} className="item">
               <ToEatCards
                 text={todo.text}
                 todoId={todo._id}
+                expirydate={todo.expirydate}
                 onRemoveTodo={onRemovedTodo}
                 wastedNum={wastedNum}
                 setWastedNum={setWastedNum}
